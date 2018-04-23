38b1f2e74dd68bd071b859931240129613bed9fd
liu <112222222222@qq.com>
2018-04-23 22:04:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fatal/andriod_rcview/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.fatal.andriod_rcview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import com.example.fatal.andriod_rcview.adapter.FruitAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import entity.Fruit;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initFruits();   //初始化水果数据
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        FruitAdapter adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initFruits() {
+        for (int i = 0; i < 2; i++) {
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.ic_launcher_background);
+            fruitList.add(apple);
+            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.ic_launcher_background);
+            fruitList.add(banana);
+            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.ic_launcher_background);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.ic_launcher_background);
+            fruitList.add(watermelon);
+            Fruit pear = new Fruit(getRandomLengthName("Pear"), R.drawable.ic_launcher_background);
+            fruitList.add(pear);
+            Fruit grape = new Fruit(getRandomLengthName("Grape"), R.drawable.ic_launcher_background);
+            fruitList.add(grape);
+            Fruit pineapple = new Fruit(getRandomLengthName("Pineapple"), R.drawable.ic_launcher_background);
+            fruitList.add(pineapple);
+            Fruit strawberry = new Fruit(getRandomLengthName("Strawberry"), R.drawable.ic_launcher_background);
+            fruitList.add(strawberry);
+            Fruit cherry = new Fruit(getRandomLengthName("Cherry"), R.drawable.ic_launcher_background);
+            fruitList.add(cherry);
+            Fruit mango = new Fruit(getRandomLengthName("Mango"), R.drawable.ic_launcher_background);
+            fruitList.add(mango);
+        }
+    }
+
+    private String getRandomLengthName(String name) {
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(name);
+        }
+        return builder.toString();
+    }
+}
